972429fd73eaa2c6e3426263cb53e94ccfdf78dd
Joe Jiang <yanliu3936@gmail.com>
2018-09-17 13:37:37 -0500

    monto carlo simulation of father son pair and histogram of calculated r for all samples (10000 samples, 25 pairs each)

--- Galton_Heights.R
+++ Galton_Heights.R
@@ -15,3 +15,22 @@ Galton_heights%>%ggplot(aes(father, son)) +
 
 Galton_heights%>%summarize(cor(father, son))
 
+B <- 10000
+N <- 25
+
+#monte carlo simulation of sampling 25 father son pairs and calculate correlation
+#between them. Notice the row sampling function sample_n()
+
+R <- replicate (B, {
+  sample_n(Galton_heights, N, replace = TRUE)%>%
+    summarize(r = cor(father, son))%>%.$r
+
+}
+                )
+
+
+# histongram of R distribution
+
+
+data.frame(R)%>%ggplot(aes(R)) + geom_histogram(bindwidth=.01, color="red")
+
